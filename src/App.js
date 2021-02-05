dc0c9fdb91d50e678ffc45f1da147041343275c7
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-02-05 12:54:17 -0500

    Fixed participant document creation bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { auth } from "database/firebase";
+import { signout } from "database";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 import Loading from "./Loading";
