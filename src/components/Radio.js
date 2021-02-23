2bbd02c34ca42bd831774f9d9ced3209883cc86e
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-02-23 15:38:14 -0500

    Refactored Radio

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -1,104 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
+import {
+  Flex,
+  Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 
-import { firestore, auth } from "database/firebase";
-import { Heading } from "@chakra-ui/react";
-import { Box, Input } from "@chakra-ui/react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css"
-
-import { Form, Button } from "views/External/Auth/Blocks";
-
-import { RadioGroup, useRadio, useRadioGroup, HStack, VStack, Flex } from "@chakra-ui/react"
-
-import { useCollection, useDocument } from "hooks";
-
-import { Spinner } from "components";
-
-function Radio(props) {
-
-    const { heading, options, init, update} = props
-
-    const formatHeading = (head) => {
-      const s = head.split('_')
-      return s[0].charAt(0).toUpperCase() + s[0].slice(1)
-    }
-
-    return (
-        <>
-          {/* <VStack> */}
-            <HStack justify='space-between'>
-            <Heading paddingBottom='5px' alignContent="center" fontSize="17px">{formatHeading(heading)}</Heading>
-            <RadioComponent heading={heading} init={init} onChange={update} options={options}/>
-            </HStack>
-          {/* </VStack> */}
-        </>
-    )
+function Radio({ name, value, options, onChange, label, error }) {
+  return (
+    <FormControl isInvalid={error}>
+      {label && <FormLabel>{label}</FormLabel>}
+      <Options>
+        {options.map((o, i) => (
+          <Button
+            key={i}
+            bg={value === o ? "blue.500" : "white"}
+            color={value === o ? "white" : "gray.500"}
+            fontWeight={value === o ? "500" : "400"}
+            colorScheme={value === o ? "blue" : "gray"}
+            borderColor={value === o ? "blue.500" : "rgb(226, 232, 240)"}
+            borderWidth="1px"
+            onClick={() => onChange(name, o)}
+            _focus={{ zIndex: 100 }}
+          >
+            {o}
+          </Button>
+        ))}
+      </Options>
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  );
 }
 
-function RadioComponent(props) {
-
-    const {heading, init, onChange} = props
-
-    const [options, setOptions] = useState(props.options)
-
-    const update = (val) => {
-      onChange(heading, val)
+const Options = styled(Flex)`
+  & > button {
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child {
+      border-top-left-radius: 0.25rem;
+      border-bottom-left-radius: 0.25rem;
     }
-  
-    const { getRootProps, getRadioProps } = useRadioGroup({
-      name: "framework",
-      defaultValue: init,
-      onChange: update
-    })
-  
-    const group = getRootProps()
-  
-    return (
-      <HStack {...group}>
-        {options.map((value) => {
-          const radio = getRadioProps({ value })
-          return (
-            <RadioCard key={value} {...radio}>
-              {value}
-            </RadioCard>
-          )
-        })}
-      </HStack>
-    )
+    &:last-child {
+      border-top-right-radius: 0.25rem;
+      border-bottom-right-radius: 0.25rem;
+    }
   }
+`;
 
-function RadioCard(props) {
-    const { getInputProps, getCheckboxProps } = useRadio(props)
-  
-    const input = getInputProps()
-    const checkbox = getCheckboxProps()
-  
-    return (
-      <Box as="label">
-        <input {...input} />
-        <Box
-        w='100px'
-        textAlign='center'
-          {...checkbox}
-          cursor="pointer"
-          borderWidth="1px"
-          borderRadius="md"
-          boxShadow="md"
-          _checked={{
-            bg: "#2E75BD",
-            color: "white",
-            borderColor: "#8EC2ED",
-          }}
-          _focus={{
-            boxShadow: "outline",
-          }}
-          px={5}
-          py={3}
-        >
-          {props.children}
-        </Box>
-      </Box>
-    )
-  }
-
-export default Radio
+export default Radio;
