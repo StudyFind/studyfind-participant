3a9f5da614f91f500e6650c06659f5f958b096f9
JeremyWebb45 <jwebb45@gatech.edu>
2021-02-25 19:12:01 -0500

    Added Signout to Settings
    See above

--- src/views/Internal/Settings/Settings.js
+++ src/views/Internal/Settings/Settings.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { signout } from "database/auth";
+import { FaDoorOpen } from "react-icons/fa";
 
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Button } from "@chakra-ui/react";
 import ChangePassword from "./ChangePassword";
 import DeleteAccount from "./DeleteAccount";
 
 function Settings() {
   return (
     <>
-      <Heading size="lg" mb="25px">
-        Settings
-      </Heading>
+      <Flex justify="space-between" align="center" mb="25px">
+        <Heading size="lg" my="8px">
+          Settings
+        </Heading>
+        <Button colorScheme="red" onClick={signout} leftIcon={<FaDoorOpen />}>
+          Sign out
+        </Button>
+      </Flex>
       <Flex gridGap="20px">
         <ChangePassword />
         <DeleteAccount />
