824aebfcd98b63b0a858eb8aecef3c3a44fb54a9
JeremyWebb45 <jwebb45@gatech.edu>
2021-02-26 15:20:36 -0500

    Added Questionnaire UI
    Yep

--- src/views/Internal/ViewStudy/Questionnaire.js
+++ src/views/Internal/ViewStudy/Questionnaire.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Spinner } from "components"
 
-import { Heading } from "@chakra-ui/react";
+import { HStack, Heading, Radio, RadioGroup, Box, Divider, Grid } from "@chakra-ui/react";
 
 
-function Questionnaire({ studies }) {
+function Questionnaire({ studies, user }) {
 
     const { nctID } = useParams();
     const findStudy = () => studies && studies.find((study) => study.id === nctID);
@@ -16,14 +17,42 @@ function Questionnaire({ studies }) {
       }
     }, [studies]);
 
+    const formatPrompt = (prompt, id) => {
+      return `${id + 1}) ${prompt}`
+    }
+
+    if (!study) return <Spinner />
+
   return (
     <>
       <Heading size="lg" mb="25px">
         Questionnaire
       </Heading>
-      <text>
-      {study.questions[0].prompt}
-      </text>
+      <Box bg="white" borderWidth="1px" rounded="md" p="20px" w="100%">
+          <Grid gap="25px">
+            {Object.entries(study.questions).map((val, i) => (
+              <>
+              <text key={i}>
+                {formatPrompt(val[1].prompt, i)}
+              </text>
+              <RadioGroup>
+                <HStack>
+                  <Radio>
+                    Yes
+                  </Radio>
+                  <Radio>
+                    No
+                  </Radio>
+                  <Radio>
+                    Unsure
+                  </Radio>
+                </HStack>
+              </RadioGroup>
+              <Divider />
+              </>
+            ))}
+          </Grid>
+          </Box>
     </>
   );
 }
