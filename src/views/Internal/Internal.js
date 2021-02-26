15a8dc38b04a6d8b1460e9fa48274695d423ba79
JeremyWebb45 <jwebb45@gatech.edu>
2021-02-25 19:54:11 -0500

    Added View Study Functionality
    See above

--- src/views/Internal/Internal.js
+++ src/views/Internal/Internal.js
@@ -4,17 +4,29 @@ import { Box, Flex } from "@chakra-ui/react";
 import { Page } from "components";
 
 import { auth, firestore } from "database/firebase";
-import { useDocument } from "hooks";
+import { useDocument, useCollection } from "hooks";
 
 import Sidebar from "./Sidebar";
 
 import Settings from "views/Internal/Settings/Settings";
 import FindStudies from "views/Internal/FindStudies/FindStudies";
 import Notifications from "views/Internal/Notifications/Notifications";
+import ViewStudy from "views/Internal/ViewStudy/ViewStudy"
 
 function Internal() {
   const { uid } = auth.currentUser;
   const [user] = useDocument(firestore.collection("participants").doc(uid));
+  const [studies] = useCollection(
+    firestore.collection("studies")
+  );
+
+  const pages = [
+    { path: "/", component: <FindStudies />},
+    { path: "/search", component: <FindStudies />},
+    { path: "/notifications", component: <Notifications />},
+    { path: "/settings", component: <Settings />},
+    { path: "/study/:nctID", component: <ViewStudy studies={studies} /> },
+  ]
 
   return (
     <Flex bg="#f8f9fa">
@@ -22,10 +34,11 @@ function Internal() {
       <Box ml="280px" w="100%" minH="100vh">
         <Page isLoading={!user}>
           <Switch>
-            <Route exact path="/" component={FindStudies} />
-            <Route exact path="/search" component={FindStudies} />
-            <Route exact path="/notifications" component={Notifications} />
-            <Route exact path="/settings" component={Settings} />
+            {pages.map(({ path, component }, index) => (
+              <Route exact path={path} key={index}>
+                {component}
+              </Route>
+            ))}
             <Redirect to="/" />
           </Switch>
         </Page>
