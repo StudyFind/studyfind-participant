6b1de1ffde7444bcbce80f1cc4615f654b1a5e01
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-06-07 22:47:41 -0400

    Removed compute export

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,4 +1,3 @@
 export { default as validate } from "./validate";
-export { default as compute } from "./compute";
 export { default as datetime } from "./datetime";
 export { default as helpers } from "./helpers";
