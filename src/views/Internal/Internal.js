3947125794517bfc48ded0477a0b3b4799369a37
DAVID CHEN <davidchen1716@gmail.com>
2021-06-08 22:42:31 -0400

    added confirm context provider

--- src/views/Internal/Internal.js
+++ src/views/Internal/Internal.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { auth, firestore } from "database/firebase";
 import { useDocument, useCollection, useDetectTimezone } from "hooks";
-import { UserContext, StudiesContext } from "context";
+import { UserContext, StudiesContext, ConfirmContext } from "context";
 
 import { Switch, Route, Redirect } from "react-router-dom";
 import { Box, Flex } from "@chakra-ui/react";
@@ -21,7 +21,7 @@ import MyStudies from "views/Internal/MyStudies/MyStudies";
 function Internal() {
   const { uid, email, emailVerified } = auth.currentUser;
 
-  const userRef = firestore.collection("researchers").doc(uid);
+  const userRef = firestore.collection("participants").doc(uid);
   const studiesRef = firestore.collection("studies");
 
   const [user] = useDocument(userRef);
@@ -34,27 +34,30 @@ function Internal() {
     <Flex>
       <UserContext.Provider value={user}>
         <StudiesContext.Provider value={studies}>
-          <Sidebar name={user && user.name} email={email} />
-          <Box
-            ml="280px"
-            w="100%"
-            minH={emailVerified ? "100vh" : "calc(100vh - 56px)"}
-            mt={emailVerified ? "" : "40px"}
-          >
-            {emailVerified || <Verification email={email} />}
-            <Page isLoading={!(user && studies)}>
-              <Switch>
-                <Route exact path="/" component={FindStudies} />
-                <Route path="/search" component={FindStudies} />
-                <Route path="/notifications" component={Notifications} />
-                <Route path="/study/:studyID/screening" component={Screening} />
-                <Route path="/study/:studyID/:tab" component={ViewStudy} />
-                <Route path="/account/:tab" component={Account} />
-                <Route path="/mystudies/:studyID?/:action?" component={MyStudies} />
-                <Redirect to="/" />
-              </Switch>
-            </Page>
-          </Box>
+          <ConfirmContext.Provider value={setConfirm}>
+            <Sidebar name={user?.name} email={email} />
+            <Box
+              ml="280px"
+              w="100%"
+              minH={emailVerified ? "100vh" : "calc(100vh - 56px)"}
+              mt={emailVerified ? "" : "40px"}
+            >
+              {emailVerified || <Verification />}
+              {confirm && <Confirm {...confirm} handleClose={() => setConfirm(null)} />}
+              <Page isLoading={!(user && studies)}>
+                <Switch>
+                  <Route exact path="/" component={FindStudies} />
+                  <Route path="/search" component={FindStudies} />
+                  <Route path="/notifications" component={Notifications} />
+                  <Route path="/study/:studyID/screening" component={Screening} />
+                  <Route path="/study/:studyID/:tab" component={ViewStudy} />
+                  <Route path="/account/:tab" component={Account} />
+                  <Route path="/mystudies/:studyID?/:action?" component={MyStudies} />
+                  <Redirect to="/" />
+                </Switch>
+              </Page>
+            </Box>
+          </ConfirmContext.Provider>
         </StudiesContext.Provider>
       </UserContext.Provider>
     </Flex>
