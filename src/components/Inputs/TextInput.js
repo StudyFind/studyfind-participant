b00480e9983c14f121375bd088fea5902f7160e2
DAVID CHEN <davidchen1716@gmail.com>
2021-06-09 01:36:16 -0400

    revert textinput for now to fix callstack overflow

--- src/components/Inputs/TextInput.js
+++ src/components/Inputs/TextInput.js
@@ -1,7 +1,25 @@
-import { Input, FormControl } from "@chakra-ui/react";
+import {
+  Input,
+  FormControl,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from "@chakra-ui/react";
 import { Label, Error } from "./helpers";
 
-export const TextInput = ({ name, value, error, label, placeholder, onChange, ...rest }) => {
+export const TextInput = ({
+  name,
+  value,
+  error,
+  label,
+  placeholder,
+  onChange,
+  left,
+  leftWidth,
+  right,
+  rightWidth,
+  ...rest
+}) => {
   const handleChange = (e) => {
     onChange(name, e.target.value);
   };
@@ -9,15 +27,27 @@ export const TextInput = ({ name, value, error, label, placeholder, onChange, ..
   return (
     <FormControl isInvalid={!!error}>
       <Label label={label} />
-      <Input
-        w="100%"
-        placeholder={placeholder}
-        _placeholder={{ color: error && "gray.500" }}
-        bg={error ? "red.100" : ""}
-        value={value}
-        onChange={handleChange}
-        {...rest}
-      />
+      <InputGroup>
+        {left && (
+          <InputLeftElement w={leftWidth} display="flex" alignItems="center" p="0px" h="100%">
+            {left}
+          </InputLeftElement>
+        )}
+        <Input
+          w="100%"
+          placeholder={placeholder}
+          _placeholder={{ color: error && "gray.500" }}
+          bg={error ? "red.100" : ""}
+          value={value}
+          onChange={handleChange}
+          {...rest}
+        />
+        {right && (
+          <InputRightElement w={rightWidth} display="flex" alignItems="center" p="0px" h="100%">
+            {right}
+          </InputRightElement>
+        )}
+      </InputGroup>
       <Error error={error} />
     </FormControl>
   );
