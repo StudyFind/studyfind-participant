929fc12256c509855340b82e7316b821a5da7943
JeremyWebb45 <jwebb45@gatech.edu>
2021-07-22 13:34:15 -0400

    MapView Centers Correctly
    Still cannot get markers to appear

--- src/views/Internal/FindStudies/MapView.js
+++ src/views/Internal/FindStudies/MapView.js
@@ -5,48 +5,154 @@ import { Loader } from "components";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { Box, Grid, Text } from "@chakra-ui/react";
 import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
-
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 function MapView({ loc, user, studies, conditions, handleConditions }) {
+
+  console.log("location", loc)
+
+  const [location, setLocation] = useState(loc);
+  const [selected, setSelected] = useState(null);
+
+  useEffect(() => setLocation(loc), [loc]);
+
+  const handleClick = (studyID) => {
+    const study = studies.find((study) => study.id === studyID);
+    console.log(study)
+    // const { locations } = study;
+
+    // if (locations) {
+    //   setLocation({
+    //     lat: locations[0].latitude,
+    //     lng: locations[0].longitude,
+    //   });
+
+    //   setSelected(study);
+    // }
+  };
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyAed_hgBp7VzxxTXlC9Buh9l_6gmNgNK1g"
   })
 
-  const [map, setMap] = React.useState(null)
+  // const [map, setMap] = React.useState(null)
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
+  // const onLoad = React.useCallback(function callback(map) {
+  //   const bounds = new window.google.maps.LatLngBounds();
+  //   map.fitBounds(bounds);
+  //   setMap(map)
+  // }, [])
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+  // const onUnmount = React.useCallback(function callback(map) {
+  //   setMap(null)
+  // }, [])
 
-  return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+  return (isLoaded && location) ? (
+    <Grid gap="25px" templateColumns="1fr 1fr">
+      <Box h="320px" w="100%" rounded="lg">
+        <GoogleMap
+          center={{lat: location.latitude, lng: location.longitude}}
+          zoom={10}
+          // onLoad={onLoad}
+          // onUnmount={onUnmount}
+        >
+          <></>
+          { /* Child components, such as markers, info windows, etc. */ }
+          {studies.map((study) => {
+            // if (study.locations.length === 0) {
+            //   return null;
+            // }
+
+            const lat = study.g.geopoint.latitude
+            const lng = study.g.geopoint.longitude
+
+            return (
+              <FaMapMarkerAlt
+                key={study.id}
+                position={{lat: lat, lng: lng}}
+                size={30}
+                color="red"
+                onClick={() => handleClick(study.id)}
+              />
+            );
+          })}
+        </GoogleMap>
+      </Box>
+      {selected && (
+        <StudyCardSmall
+          study={selected}
+          user={user}
+          conditions={conditions}
+          handleConditions={handleConditions}
+        />
+      )}
+    </Grid>
+  ) : <Loader/>
 }
+
+// function MapView({ loc, user, studies, conditions, handleConditions }) {
+//   const [location, setLocation] = useState(loc);
+//   const [selected, setSelected] = useState(null);
+
+//   useEffect(() => setLocation(loc), [loc]);
+
+//   const handleClick = (studyID) => {
+//     const study = studies.find((study) => study.id === studyID);
+//     const { locations } = study;
+
+//     if (locations) {
+//       setLocation({
+//         lat: locations[0].latitude,
+//         lng: locations[0].longitude,
+//       });
+
+//       setSelected(study);
+//     }
+//   };
+
+//   if (!location) return <Loader />;
+
+//   return (
+//     // Important! Always set the container height explicitly
+//     <Grid gap="25px" templateColumns="1fr 1fr">
+//       <Box h="320px" w="100%" rounded="lg">
+//         <GoogleMapReact
+//           bootstrapURLKeys={{ key: "AIzaSyAed_hgBp7VzxxTXlC9Buh9l_6gmNgNK1g" }}
+//           center={location}
+//           zoom={11}
+//         >
+//           {studies.map((study) => {
+//             if (study.locations.length === 0) {
+//               return null;
+//             }
+
+//             const lat = study.locations[0].latitude;
+//             const lng = study.locations[0].longitude;
+
+//             return (
+//               <FaMapMarkerAlt
+//                 key={study.id}
+//                 lat={lat}
+//                 lng={lng}
+//                 size={30}
+//                 color="red"
+//                 onClick={() => handleClick(study.id)}
+//               />
+//             );
+//           })}
+//         </GoogleMapReact>
+//       </Box>
+//       {selected && (
+//         <StudyCardSmall
+//           study={selected}
+//           user={user}
+//           conditions={conditions}
+//           handleConditions={handleConditions}
+//         />
+//       )}
+//     </Grid>
+//   );
+// }
 
 export default React.memo(MapView);
