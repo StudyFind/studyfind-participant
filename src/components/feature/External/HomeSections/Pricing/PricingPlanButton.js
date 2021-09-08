f3321ad1275e9e84cf1800e0dd003c9c68902c2e
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-07 21:39:54 -0400

    Updating feature components

--- src/components/feature/External/HomeSections/Pricing/PricingPlanButton.js
+++ src/components/feature/External/HomeSections/Pricing/PricingPlanButton.js
@@ -1,13 +1,19 @@
+import { useHistory } from "hooks";
+
 import { Button } from "@chakra-ui/react";
-import { Link } from "components";
 
 function PricingPlanButton() {
+  const history = useHistory();
+
+  const handleClick = () => {
+    localStorage.setItem("redirect", "/account/subscription");
+    history.push("/auth");
+  };
+
   return (
-    <Link to="/auth">
-      <Button size="lg" width="100%" marginTop="10px" colorScheme="blue">
-        Start Now
-      </Button>
-    </Link>
+    <Button size="lg" width="100%" marginTop="10px" colorScheme="blue" onClick={handleClick}>
+      Start Now
+    </Button>
   );
 }
 
